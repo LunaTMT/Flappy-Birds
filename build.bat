46e6cd5500b3f59813ba3f32c617b345fcf5404a
Маршалл <44672093+LunaTMT@users.noreply.github.com>
2024-03-13 21:24:42 +0000

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,8 +4,7 @@ cls
 echo Compiling C++ code...
 
 :: Create 'bin' directory if it doesn't exist
-if not exist bin mkdir bin
-
+if not exist bin m
 :: Compile C++ code
 g++ -g src\*.cpp src\entities\*.cpp src\utilities\*.cpp src\globals\*.cpp -Iinclude -Iinclude\entities -Iinclude\utilities -Iinclude\globals -o bin\main -lsfml-graphics -lsfml-window -lsfml-system
 
